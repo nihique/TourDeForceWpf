b10ede14422f6ecadad82a334bb318dadca7066d
nihique <kovarik.martin@gmail.com>
2012-12-19 23:12:16 +0100

    button logic

--- TourDeForceWpf/MainWindow.xaml.cs
+++ TourDeForceWpf/MainWindow.xaml.cs
@@ -1,37 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using IdeaBlade.EntityModel;
+﻿using System.Windows;
 
 namespace TourDeForceWpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private MainWindowViewModel _vm;
+
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += delegate { DataContext = new MainWindowViewModel(); };
         }
 
-        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
-            System.Windows.Data.CollectionViewSource employeeViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("employeeViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // employeeViewSource.Source = [generic data source]
+            _vm = new MainWindowViewModel();
+            DataContext = _vm;
+            _vm.PropertyChanged += delegate { EnableDisableButtons(); };
+            _vm.Employees.CollectionChanged += delegate { EnableDisableButtons(); };
         }
+
+        private void First_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.MoveCurrentToFirst();
+        }
+
+        private void Previous_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.MoveCurrentToPrevious();
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.MoveCurrentToNext();
+        }
+
+        private void Last_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.MoveCurrentToLast();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.AddNew();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (_vm.CurrentPosition > -1) _vm.RemoveAt(_vm.CurrentPosition);
+        }
+
+        private void EnableDisableButtons()
+        {
+            First.IsEnabled = _vm.CurrentPosition > 0;
+            Previous.IsEnabled = _vm.CurrentPosition > 0;
+            Next.IsEnabled = _vm.CurrentPosition < _vm.Employees.Count - 1;
+            Last.IsEnabled = _vm.CurrentPosition < _vm.Employees.Count - 1;
+            Add.IsEnabled = _vm.Employees != null;
+            Delete.IsEnabled = _vm.CurrentPosition > -1;
+            Ok.IsEnabled = _vm.CanSave;
+            Cancel.IsEnabled = _vm.CanSave;
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.Save();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.RollbackChanges();
+        }
+
     }
 }
--- TourDeForceWpf/MainWindowViewModel.cs
+++ TourDeForceWpf/MainWindowViewModel.cs
@@ -1,29 +1,159 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using IdeaBlade.Core;
+using IdeaBlade.EntityModel;
+using TourDeForceWpf.Annotations;
 
 namespace TourDeForceWpf
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<Employee> Employees { get; private set; }
+        private Employee _currentEmployee;
+        private int _currentPosition;
+        private NorthwindIBEntities _entities;
+        private bool _canSave;
+        private EntityQuery<Employee> _query;
 
         public MainWindowViewModel()
         {
             MainWindowViewModelAsync();
         }
 
+        public ObservableCollection<Employee> Employees { get; private set; }
+
+        public Employee CurrentEmployee
+        {
+            get { return _currentEmployee; }
+            set
+            {
+                _currentEmployee = value;
+                OnPropertyChanged("CurrentEmployee");
+            }
+        }
+
+        public bool CanSave
+        {
+            get { return _canSave; }
+            set
+            {
+                _canSave = value;
+                OnPropertyChanged("CanSave");
+            }
+        }
+
+        public int CurrentPosition
+        {
+            get { return _currentPosition; }
+            set
+            {
+                if (Employees != null && Employees.Count > 0)
+                {
+                    _currentPosition = Math.Min(Math.Max(value, 0), Employees.Count - 1);
+                    CurrentEmployee = Employees[_currentPosition];
+                }
+                _currentPosition = value;
+                OnPropertyChanged("CurrentPosition");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         private async void MainWindowViewModelAsync()
         {
             Employees = new ObservableCollection<Employee>();
-            var mgr = new NorthwindIBEntities();
-            var query = mgr.Employee;
-            var results = await mgr.ExecuteQueryAsync(query);
+            _entities = new NorthwindIBEntities();
+            _query = _entities.Employee;
+            IEnumerable<Employee> results = await _entities.ExecuteQueryAsync(_query);
             results.ForEach(Employees.Add);
+            MoveCurrentToFirst();
+            _entities.EntityChanged += (sender, args) =>
+                {
+                    CanSave = ((NorthwindIBEntities) sender).HasChanges();
+                };
+            Employees.CollectionChanged += Employees_CollectionChanged;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Employees_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action) 
+            {
+                case NotifyCollectionChangedAction.Add:
+                    Add(e.NewItems.Cast<Employee>());
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    Remove(e.OldItems.Cast<Employee>());
+                    break;
+            }
+        }
+
+        public void RollbackChanges()
+        {
+            int privateCurrentPosition = CurrentPosition;
+            _entities.RejectChanges();
+            Employees.Clear();
+            _query.With(QueryStrategy.CacheOnly).Execute().ForEach(Employees.Add);
+            CurrentPosition = privateCurrentPosition;
+        }
+
+        private void Remove(IEnumerable<Employee> employees)
+        {
+            employees.ForEach(x => x.EntityAspect.Delete());
+            Save();
+        }
+
+        private void Add(IEnumerable<Employee> employees)
+        {
+            _entities.AddEntities(employees);
+        }
+
+        public void MoveCurrentToFirst()
+        {
+            CurrentPosition = 0;
+        }
+
+        public void MoveCurrentToPrevious()
+        {
+            CurrentPosition -= 1;
+        }
+
+        public void MoveCurrentToNext()
+        {
+            CurrentPosition += 1;
+        }
+
+        public void MoveCurrentToLast()
+        {
+            CurrentPosition = Employees != null ? Employees.Count - 1 : -1;
+        }
+
+        public void AddNew()
+        {
+            Employees.Add(new Employee());
+            MoveCurrentToLast();
+        }
+
+        public void RemoveAt(int position)
+        {
+            Employees.RemoveAt(position);
+            CurrentPosition = position;
+        }
+
+        public void Save()
+        {
+            _entities.SaveChanges();
         }
     }
 }
